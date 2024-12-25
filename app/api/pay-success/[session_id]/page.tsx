888cfa60390146723edf58a1e7900d59ced0b3bf
zhiyu <zhiyu@it.com>
2024-12-25 22:52:46 +0800

    重新上传

--- app/api/pay-success/[session_id]/page.tsx
+++ app/api/pay-success/[session_id]/page.tsx
@@ -1,26 +1,14 @@
 import { updateOrderStatus } from "@/models/order";
+import { handleOrderSession } from "@/service/order";
 import { redirect } from "next/navigation";
 import Stripe from "stripe";
 
 export default async function ({ params }: { params: { session_id: string } }) {
-  console.log("pay id ", params.session_id);
-
-  const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY || "");
   try {
-    const session = await stripe.checkout.sessions.retrieve(params.session_id);
-    console.log("order session: ", session);
-    if (!session || !session.metadata || !session.metadata.order_no) {
-      console.log("invalid session", params.session_id);
-      throw new Error("invalid session");
-    }
-
-    const order_no = session.metadata.order_no;
-    const paied_at = new Date().toISOString();
-    updateOrderStatus(order_no, 2, paied_at);
-    console.log("update success order status: ", order_no, paied_at);
-    redirect("/");
+    handleOrderSession(params.session_id);
   } catch (e) {
     console.log("handle order session failed: ", e);
     throw e;
   }
+  redirect("/");
 }
--- service/order.ts
+++ service/order.ts
@@ -1,7 +1,28 @@
-import { getUserOrders } from "@/models/order";
+import { getUserOrders, updateOrderStatus } from "@/models/order";
 import { getUserWallpapersCounts } from "@/models/wallpaper";
 import { Order } from "@/types/order";
 import { UserCredits } from "@/types/user";
+import Stripe from "stripe";
+
+export async function handleOrderSession(session_id: string) {
+  const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY || "");
+  try {
+    const session = await stripe.checkout.sessions.retrieve(session_id);
+    console.log("order session: ", session);
+    if (!session || !session.metadata || !session.metadata.order_no) {
+      console.log("invalid session", session_id);
+      throw new Error("invalid session");
+    }
+
+    const order_no = session.metadata.order_no;
+    const paied_at = new Date().toISOString();
+    updateOrderStatus(order_no, 2, paied_at);
+    console.log("update success order status: ", order_no, paied_at);
+  } catch (e) {
+    console.log("handle order session failed: ", e);
+    throw e;
+  }
+}
 
 export async function getUserCredits(user_email: string): Promise<UserCredits> {
   //初始化用户积分
